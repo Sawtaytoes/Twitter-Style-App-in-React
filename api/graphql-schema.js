d2433426d4222409231ee55d513eb8af1e9e66b1
Kevin Ghadyani <Saturn2888@gmail.com>
2016-12-14 20:28:21 -0600

    Added GraphQL mutations to add a user and tweet

--- api/graphql-schema.js
+++ api/graphql-schema.js
@@ -27,6 +27,23 @@ const resolvers = {
 		}),
 	},
 
+	Mutation: {
+		addTweet: (_, { input }) => Tweets.add({
+			[Tweets.schema.userId]: input.userId,
+			[Tweets.schema.content]: input.content,
+		}),
+		updateTweet: (_, { input }) => Tweets.update(input.tweetId, {
+			[Tweets.schema.content]: input.content,
+		}),
+		addUser: (_, { input }) => Users.add({
+			[Users.schema.username]: input.username,
+			[Users.schema.password]: input.password,
+		}),
+		updateUser: (_, { input }) => Users.update(input.userId, {
+			[Users.schema.displayName]: input.displayName,
+		}),
+	},
+
 	User: {
 		tweets: ({ userId }) => Tweets.getAllByUserId(userId),
 		joinTime: ({ joinTime }) => convertToDateTime(joinTime),
